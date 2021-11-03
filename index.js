6ce14b867119120d050402f81c833239e006fb5a
michaelm3 <michael.muniz94@gmail.com>
2021-11-03 15:21:24 -0400

    last commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
+const countdownContainer = document.querySelector('.countdown-container')
 
 const arrival = 'November 4, 2021 14:20:00';
 
@@ -9,22 +10,31 @@ function countdown() {
     const arrivalDate = new Date(arrival);
     const currentDate = new Date();
 
-    const totalSeconds = (arrivalDate - currentDate) / 1000
+    if (currentDate < arrivalDate) {
 
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const minutes = Math.floor(totalSeconds / 60) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
+        const totalSeconds = (arrivalDate - currentDate) / 1000
 
-    daysEl.innerHTML = days;
-    hoursEl.innerHTML = formatTime(hours);
-    minutesEl.innerHTML = formatTime(minutes);
-    secondsEl.innerHTML = formatTime(seconds);
+        const days = Math.floor(totalSeconds / 3600 / 24);
+        const hours = Math.floor(totalSeconds / 3600) % 24;
+        const minutes = Math.floor(totalSeconds / 60) % 60;
+        const seconds = Math.floor(totalSeconds) % 60;
+
+        daysEl.innerHTML = days;
+        hoursEl.innerHTML = formatTime(hours);
+        minutesEl.innerHTML = formatTime(minutes);
+        secondsEl.innerHTML = formatTime(seconds);
+    } else {
+        celebrate()
+    }
 
 }
 
 function formatTime(time) {
     return time < 10 ? (`0${time}`) : time;
+}
+
+function celebrate() {
+    return countdownContainer.innerHTML = `<h1>I LOVE YOU</h1>`
 }
 
 // Initial call
